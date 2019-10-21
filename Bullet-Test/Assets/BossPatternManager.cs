5b8a9f9a5416542510bee93b61618b45dbcead79
Nicolas Ezequiel Puccio <nico.puccio@hotmail.com>
2019-10-21 18:31:18 -0300

    Added a new scene and some scripts to start testing some stuff for the BossPatternManager

---
+++ Bullet-Test/Assets/BossPatternManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossPatternManager : MonoBehaviour {
+
+    public Boss boss;
+    public bool flowerPattern = true; 
+    
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (boss.health < 100)
+        {
+            flowerPattern = false;
+        }
+        else
+        {
+            flowerPattern = true;
+        }
+	}
+}
