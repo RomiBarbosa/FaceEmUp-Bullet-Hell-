ea889285c8f000fb7d405019238776227fc95eca
Nicolas Ezequiel Puccio <nico.puccio@hotmail.com>
2019-10-28 12:05:29 -0300

    Added box colliders to bullets and boss.

--- Bullet-Test/Assets/Scripts/BulletEnemy.cs
+++ Bullet-Test/Assets/Scripts/BulletEnemy.cs
@@ -5,12 +5,22 @@ using UnityEngine;
 public class BulletEnemy : MonoBehaviour {
 
 	
-	void Start () {
+	void Start ()
+    {
         Destroy(gameObject,3);
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+        
 	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Boss")
+        {
+            Destroy(gameObject);
+        }
+    }
 }
