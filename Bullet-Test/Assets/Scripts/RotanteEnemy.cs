f46890cdbc6995a6d4878aea162f5d26941ff9af
Romibarbosa <romy_jul@hotmail.com>
2019-10-31 12:46:11 -0300

    Scene "SampleScene" with new patterns, reduce size of heart

---
+++ Bullet-Test/Assets/Scripts/RotanteEnemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotanteEnemy : MonoBehaviour {
+
+    public GameObject Target;
+    public float speed ;
+
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+        Rotate();
+
+    }
+
+    void Rotate()
+    {
+
+        transform.RotateAround(Target.transform.position,Vector3.forward, speed * Time.deltaTime);
+    }
+}
