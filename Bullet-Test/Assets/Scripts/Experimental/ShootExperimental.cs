716d79ef20c828234265fcaafb0d1e88132ed3b5
Romibarbosa <romy_jul@hotmail.com>
2019-11-01 15:09:28 -0300

    Bullet follow the player

--- Bullet-Test/Assets/Scripts/Experimental/ShootExperimental.cs
+++ Bullet-Test/Assets/Scripts/Experimental/ShootExperimental.cs
@@ -1,8 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ShootExperimental : MonoBehaviour {
 
-   
+    Rigidbody2D rb;
+    GameObject target;
+    float movespeed;
+    Vector2 directiontoTarget;
+
+    private void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+        rb = GetComponent<Rigidbody2D>();
+        movespeed = 5f;
+        directiontoTarget = (target.transform.position - transform.position).normalized * movespeed;
+        rb.velocity = new Vector2(directiontoTarget.x, directiontoTarget.y);
+    }
+  
 }
