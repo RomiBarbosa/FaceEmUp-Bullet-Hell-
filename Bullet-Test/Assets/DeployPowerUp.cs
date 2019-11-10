adc7d78774ba35b9feff6b364639df9309569d34
Nicolas Ezequiel Puccio <nico.puccio@hotmail.com>
2019-11-09 22:47:54 -0300

    Power Up spawns. Enemies now have a chance to drop a random powerup.

---
+++ Bullet-Test/Assets/DeployPowerUp.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DeployPowerUp : MonoBehaviour {
+
+
+    private System.Random rnd = new System.Random();
+    private System.Random rndIndex = new System.Random();
+
+    private int powerUpChance = 10;
+    public GameObject[] powerups;
+
+    
+
+    private void InstantiatePowerUp()
+    {
+        Vector2 position = transform.position;
+        var chance = rnd.Next(0, 101);
+        if (chance < powerUpChance)
+        {
+            var i = rndIndex.Next(1, 5);
+            Instantiate(powerups[i - 1], position, Quaternion.identity);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        InstantiatePowerUp();
+    }
+
+}
