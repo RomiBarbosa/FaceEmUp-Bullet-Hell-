e620963227ef6923f3389b730780013ea5057e9b
Romibarbosa <romy_jul@hotmail.com>
2019-11-11 21:12:12 -0300

    Scene PatronesBoss with new patterns

---
+++ Bullet-Test/Assets/Scripts/TEST.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEST : MonoBehaviour {
+
+    public GameObject[] Wave;
+    public float Rate;
+    float next;
+    public bool spawn;
+    int index;
+
+    public void Update()
+    {
+        if (index < Wave.Length)
+        {
+
+            if (spawn == true)
+            {
+
+                if (Time.time > next)
+                {
+                    Wave[index].SetActive(true);
+                    index++;
+                    for (int i = 0; i < Wave.Length; i++)
+                    {
+                        if (Wave[index] != null)
+                        {
+                            Wave[index].SetActive(false);
+
+                        }
+                       
+                    }
+                    next = Time.time + Rate;
+                }
+            }
+        }
+     
+
+    }
+}
