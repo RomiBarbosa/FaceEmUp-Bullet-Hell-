3b02a8b371ea0a81c34037cecc3553be986634e1
Romibarbosa <romy_jul@hotmail.com>
2019-11-17 13:16:03 -0300

    Win panel with color, fix problem with menu

---
+++ Bullet-Test/Assets/Scripts/Settings/flashing.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class flashing : MonoBehaviour {
+
+    public Image theImage;
+
+	// Update is called once per frame
+	void Update () {
+
+        if (theImage !=null)
+        {
+            Color newcolor = new Color(Random.value,Random.value,Random.value);
+            theImage.color = newcolor;
+
+        }
+	}
+}
